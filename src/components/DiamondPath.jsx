4c28021a44de9a4c9760a7ba35925669cc0a59af
Talha Qureshi <talha_qureshi@hotmail.com>
2022-04-28 15:44:20 +0500

    nicer diamond path

--- src/components/DiamondPath.jsx
+++ src/components/DiamondPath.jsx
@@ -4,7 +4,7 @@ export default function DiamondPath(props) {
   return (
     <svg width="600" height="600" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
 			<defs>
-			<path id="path1" d="M10 110 230 110M210 90H30M50 70H190M170 50H70M90 30H150M130 10H110M30 130H210M190 150H50M70 170H170M150 190H90M110 210H130"></path>
+			<path id="path1" d="M180 11H210M150 41H240M120 71H270M90 101H300M60 131H330M30 161H360M60 191H330M90 221H300M120 251H270M150 281H240M180 311H210"></path>
 			</defs>
 			<text transform="translate(0,35)" fill="red" fontSize="8">
 			<textPath xlinkHref="#path1"> {props.text} </textPath>
